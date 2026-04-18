95ff7fd019c4f089500b06240fee7f1aee423ef5
alokbhanawat <alokbhanawat@gmail.com>
2026-04-18 11:37:53 +0530

    day 4 task completed

--- backend/src/main/java/com/infosys/backend/repository/UserRepository.java
+++ backend/src/main/java/com/infosys/backend/repository/UserRepository.java
@@ -1,3 +1,7 @@
-public class UserRepository {
-    
+package com.infosys.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.infosys.backend.model.*;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
